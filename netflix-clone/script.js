f167ebc92ebda9ef2a6beba662ecf43982d221c8
Chamath Herath <chamathherath253@gmail.com>
2026-08-20 21:14:17 +0530

    Updated js file

--- netflix-clone/script.js
+++ netflix-clone/script.js
@@ -1,52 +1,49 @@
-// ================= SEARCH FUNCTION =================
-
 const searchInput = document.getElementById("searchInput");
 
-const movieCards = document.querySelectorAll(".movie-card");
+if (searchInput) {
 
-const noResults = document.getElementById("noResults");
+    const movieCards = document.querySelectorAll(".movie-card");
+    const noResults = document.getElementById("noResults");
+
+    searchInput.addEventListener("input", function () {
+
+        const searchText =
+            searchInput.value.toLowerCase().trim();
+
+        let foundMovies = 0;
+
+        movieCards.forEach(function (card) {
+
+            const movieName =
+                card.querySelector("h3")
+                    .textContent
+                    .toLowerCase();
+
+            if (movieName.includes(searchText)) {
+
+                card.style.display = "block";
+
+                foundMovies++;
+
+            } else {
+
+                card.style.display = "none";
+
+            }
+
+        });
 
 
-searchInput.addEventListener("input", function () {
+        if (foundMovies === 0 && searchText !== "") {
 
-    const searchText = searchInput.value.toLowerCase().trim();
-
-    let foundMovies = 0;
-
-
-    movieCards.forEach(function (card) {
-
-        const movieName = card
-            .querySelector("h3")
-            .textContent
-            .toLowerCase();
-
-
-        if (movieName.includes(searchText)) {
-
-            card.style.display = "block";
-
-            foundMovies++;
+            noResults.style.display = "block";
 
         } else {
 
-            card.style.display = "none";
+            noResults.style.display = "none";
 
         }
 
     });
 
-
-    // Show no results message
-
-    if (foundMovies === 0 && searchText !== "") {
-
-        noResults.style.display = "block";
-
-    } else {
-
-        noResults.style.display = "none";
-
-    }
-
-});
+}
